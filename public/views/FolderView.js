4f6f4a241f84d7a2e750b8da2aeb9816f9a33199
ztigma <48769371+ztigma@users.noreply.github.com>
2026-04-11 03:50:06 -0400

    cv

---
+++ public/views/FolderView.js
@@ -0,0 +1,74 @@
+class FolderView extends Vista
+{
+    constructor(tag)
+    {
+        super(tag);
+        /**
+         * @type {Array<String>}
+         */
+        this.rutas = [];//yes!!! YES YES YESYEYEYSYSYES!
+    }
+    async Render(spot)
+    {
+        let i = 0;
+        let seguirCargando = true;
+
+        while(seguirCargando)
+        {
+            let name = `${i}.jpg`;
+            let img = new Image();
+            let path = `${spot}/${name}` 
+            img.src = path;
+
+            let b = await new Promise
+            (
+                resolve => 
+                {
+                    img.onload = () => resolve(true);
+                    img.onerror = () => resolve(false);
+                }
+            );
+
+            if(b)
+            {
+                this.rutas.push(path);
+                i++;
+            }
+            else
+            {
+                seguirCargando = false;
+            }
+        }
+        console.log(`i: ${i}`);
+        return this.rutas;//???
+    }
+}
+
+
+/**
+
+async function renderizarHastaElFin() {
+    const contenedor = document.getElementById('galeria-seikan');
+    let i = 1;
+    let seguirCargando = true;
+
+    while (seguirCargando) {
+        const nombreImagen = `${i}.jpg`;
+        const img = new Image();
+        img.src = `./public/imagenes/${nombreImagen}`;
+
+        // Intentamos cargar la imagen
+        const existe = await new Promise(resolve => {
+            img.onload = () => resolve(true);
+            img.onerror = () => resolve(false);
+        });
+
+        if (existe) {
+            contenedor.appendChild(img);
+            i++;
+        } else {
+            seguirCargando = false; // El bucle se rompe cuando no encuentra la siguiente
+        }
+    }
+}
+ */
