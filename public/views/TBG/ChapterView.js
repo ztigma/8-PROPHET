771957e3960745b9795e43057aa3e9c2b2bebd36
ztigma <48769371+ztigma@users.noreply.github.com>
2026-05-29 09:00:30 -0400

    Add files via upload

---
+++ public/views/TBG/ChapterView.js
@@ -0,0 +1,88 @@
+let gallery_view = 
+{
+    path:[]
+    ,
+    views:[]
+}
+console.log('Iniciando carga');
+
+var parametros = new URLSearchParams(window.location.search);
+var chapter = parametros.get('ch');
+
+async function start (params) 
+{
+    let r = await new FolderView().Render(`${chapter}`);//PERFECT!
+    
+    gallery_view.paths = r;
+    
+    console.log('carga terminada');
+    console.log(r); //Nice Nice NIce! NICE!!!
+
+    let i = 0;
+
+    r.forEach
+    (
+        p =>
+        {
+            gallery_view.views.push
+            (
+                new Vista('img').w //JAJAJA HAHAHAHAHAH KAKAKAKA!
+                .set_attrs
+                ({
+                    src:`${p}`
+                })
+                .set_styles
+                ({
+                    width:'100%'
+                    ,
+                    margin:'0'
+                    ,
+                })
+            )
+            i++;    
+        }
+    )
+
+    document.body.set_childrens
+    ([
+        new Vista('a').w
+        .set_attrs
+        ({
+            onclick:
+            `
+                window.history.back()
+            `
+            ,
+            //href:'/'
+        })
+        .set_childrens
+        ([
+            ' 戻る '
+        ])
+    ])
+
+    document.body.set_childrens(gallery_view.views);
+
+    document.body.set_childrens
+    ([
+        new Vista('a').w
+        .set_attrs
+        ({
+            onclick:
+            `
+                window.history.back()
+            `
+            ,
+            //href:'/'
+        })
+        .set_childrens
+        ([
+            ' 戻る '
+        ])
+    ])
+
+    console.log('vista colocada');
+
+    is_loading = false;
+}
+start();
